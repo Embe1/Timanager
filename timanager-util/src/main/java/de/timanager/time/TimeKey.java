d8dff0a3ae9bb2a0408ca4dea34e1f6acf6a4d9c
Michael Becker <michael.becker@enghouse.com>
2020-06-08 10:27:41 +0200

    Year-overview running now.

--- timanager-util/src/main/java/de/timanager/time/TimeKey.java
+++ timanager-util/src/main/java/de/timanager/time/TimeKey.java
@@ -1,8 +1,11 @@
 package de.timanager.time;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDateTime;
 import java.util.HashMap;
 
+@Slf4j
 public enum TimeKey {
     WORKTIME_START,
     WORKTIME_END,
--- timanager-util/src/main/java/de/timanager/time/TimeUtils.java
+++ timanager-util/src/main/java/de/timanager/time/TimeUtils.java
@@ -3,52 +3,76 @@ package de.timanager.time;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class TimeUtils {
+
+    public static List<LocalDateTime> getListOfCompleteDayRecords(TimeMap<String, LocalDateTime> timeMap, Month month) {
+        List<LocalDateTime> worktimeStart = getAllTimesOfTimeKey(timeMap, TimeKey.WORKTIME_START, month);
+        List<LocalDateTime> worktimeEnd = getAllTimesOfTimeKey(timeMap, TimeKey.WORKTIME_END, month);
+        List<LocalDateTime> lunchtimeStart = getAllTimesOfTimeKey(timeMap, TimeKey.LUNCH_START, month);
+        List<LocalDateTime> lunchtimeEnd = getAllTimesOfTimeKey(timeMap, TimeKey.LUNCH_END, month);
+
+        if (worktimeStart.size() <= worktimeEnd.size() && worktimeStart.size() <= lunchtimeStart.size() && worktimeStart.size() <= lunchtimeEnd.size())
+            return worktimeStart;
+        else if (worktimeEnd.size() <= worktimeStart.size() && worktimeEnd.size() <= lunchtimeStart.size() && worktimeEnd.size() <= lunchtimeEnd.size())
+            return worktimeEnd;
+        else if (lunchtimeStart.size() <= worktimeStart.size() && lunchtimeStart.size() <= worktimeEnd.size() && lunchtimeStart.size() <= lunchtimeEnd.size())
+            return lunchtimeStart;
+        else
+            return lunchtimeEnd;
+    }
 
     /**
      * Counts the amount of an permitted key in this map.
      *
      * @param timeKey the key to search for.
-     * @return
+     * @return a list, with all times, found in the actual month-time-map.
      */
-    public static int getCountOfKey(TimeMap<String, LocalDateTime> timeMap, TimeKey timeKey, Month month) {
-        int countedEntries = 0;
+    private static List<LocalDateTime> getAllTimesOfTimeKey(TimeMap<String, LocalDateTime> timeMap, TimeKey timeKey, Month month) {
+        List<LocalDateTime> list = new ArrayList<>();
         LocalDate localDate = LocalDate.of(LocalDate.now().getYear(), month, LocalDate.now().getDayOfMonth());
 
         for (int i = 1; i < TimeUtils.getDayCountOfSpecificMonth(localDate); i++) {
-            LocalDateTime value = timeMap.get(timeKey.generateKey(LocalDateTime.of(
-                    LocalDate.now().getYear(),
-                    month,
-                    i,
-                    LocalTime.now().getHour(),
-                    LocalTime.now().getMinute(),
-                    LocalTime.now().getSecond(),
-                    LocalTime.now().getNano())));
+            LocalDateTime value = timeMap.get(
+                    timeKey.generateKey(
+                            LocalDateTime.of(
+                                    LocalDate.now().getYear(),
+                                    month,
+                                    i,
+                                    LocalTime.now().getHour(),
+                                    LocalTime.now().getMinute(),
+                                    LocalTime.now().getSecond(),
+                                    LocalTime.now().getNano()
+                            )
+                    )
+            );
 
             if (value != null) {
-                countedEntries++;
-                log.error(value.toString());
+                list.add(value);
             }
         }
-        return countedEntries;
+        return list;
+    }
+
+    /**
+     * Get the number of days in that month
+     *
+     * @return the amount of days in the permitted month.
+     */
+    public static int getDayCountOfSpecificMonth(LocalDate date) {
+        return YearMonth.of(date.getYear(), date.getMonth()).lengthOfMonth();
     }
 
     /**
      * Returns the amount of days of the permitted month.
+     *
      * @param month
      * @return
      */
     public final int getDaysOfMonth(Month month) {
         return 0;
-    }
-
-    /**
-     * Get the number of days in that month
-     * @return the amount of days in the permitted month.
-     */
-    public static int getDayCountOfSpecificMonth(LocalDate date) {
-        return YearMonth.of(date.getYear(), date.getMonth()).lengthOfMonth();
     }
 }
